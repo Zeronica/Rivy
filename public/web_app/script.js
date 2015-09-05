6680196fef778dc2973eb318bf1d83439f6fc242
Tony Nie <nieziran1996@gmail.com>
2015-09-05 16:16:33 -0700

    initial new View commit

--- public/web_app/script.js
+++ public/web_app/script.js
@@ -3,20 +3,21 @@
 // has dependent on mobile-angular-ui
 // 
 var app = angular.module('MobileAngularUiExamples', [
-  'ngRoute',
-  'mobile-angular-ui',
-  
-  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
-  // it is at a very beginning stage, so please be careful if you like to use
-  // in production. This is intended to provide a flexible, integrated and and 
-  // easy to use alternative to other 3rd party libs like hammer.js, with the
-  // final pourpose to integrate gestures into default ui interactions like 
-  // opening sidebars, turning switches on/off ..
-  'mobile-angular-ui.gestures'
-]);
+	'ngRoute',
+	'mobile-angular-ui',
+	'myApp',
+	
+	// touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+	// it is at a very beginning stage, so please be careful if you like to use
+	// in production. This is intended to provide a flexible, integrated and and 
+	// easy to use alternative to other 3rd party libs like hammer.js, with the
+	// final pourpose to integrate gestures into default ui interactions like 
+	// opening sidebars, turning switches on/off ..
+	'mobile-angular-ui.gestures'
+	]);
 
 app.run(function($transform) {
-  window.$transform = $transform;
+	window.$transform = $transform;
 });
 
 // 
@@ -25,97 +26,97 @@ app.run(function($transform) {
 // in order to avoid unwanted routing.
 // 
 app.config(function($routeProvider) {
-  $routeProvider.when('/',              {templateUrl: '/templates/home.html', reloadOnSearch: false});
-  $routeProvider.when('/scroll',        {templateUrl: '/templates/scroll.html', reloadOnSearch: false}); 
-  $routeProvider.when('/toggle',        {templateUrl: '/templates/toggle.html', reloadOnSearch: false}); 
-  $routeProvider.when('/tabs',          {templateUrl: '/templates/tabs.html', reloadOnSearch: false}); 
-  $routeProvider.when('/accordion',     {templateUrl: '/templates/accordion.html', reloadOnSearch: false}); 
-  $routeProvider.when('/overlay',       {templateUrl: '/templates/overlay.html', reloadOnSearch: false}); 
-  $routeProvider.when('/forms',         {templateUrl: '/templates/forms.html', reloadOnSearch: false});
-  $routeProvider.when('/dropdown',      {templateUrl: '/templates/dropdown.html', reloadOnSearch: false});
-  $routeProvider.when('/touch',         {templateUrl: '/templates/touch.html', reloadOnSearch: false});
-  $routeProvider.when('/swipe',         {templateUrl: '/templates/swipe.html', reloadOnSearch: false});
-  $routeProvider.when('/drag',          {templateUrl: '/templates/drag.html', reloadOnSearch: false});
-  $routeProvider.when('/drag2',         {templateUrl: '/templates/drag2.html', reloadOnSearch: false});
-  $routeProvider.when('/carousel',      {templateUrl: '/templates/carousel.html', reloadOnSearch: false});
+	$routeProvider.when('/',              {templateUrl: '/templates/pages/home.html', reloadOnSearch: false});
+    $routeProvider.when('/my_rivys',              {templateUrl: '/templates/pages/my_rivys.html', reloadOnSearch: false});
+    $routeProvider.when('/account',              {templateUrl: '/templates/pages/account.html', reloadOnSearch: false});
+    $routeProvider.when('/location_profile/:location_id', {
+        templateUrl: '/templates/pages/location_profile.html',
+        reloadOnSearch: false
+    });
+    $routeProvider.when('/new_rivy', {templateUrl: '/templates/pages/new_rivy.html', reloadOnSearch: false});
 });
+
+
+// ==================================================================================
+// everything below is default example code and has been untouched
+// ==================================================================================
 
 // 
 // `$touch example`
 // 
 
 app.directive('toucharea', ['$touch', function($touch){
-  // Runs during compile
-  return {
-    restrict: 'C',
-    link: function($scope, elem) {
-      $scope.touch = null;
-      $touch.bind(elem, {
-        start: function(touch) {
-          $scope.touch = touch;
-          $scope.$apply();
-        },
+	// Runs during compile
+	return {
+		restrict: 'C',
+		link: function($scope, elem) {
+			$scope.touch = null;
+			$touch.bind(elem, {
+				start: function(touch) {
+					$scope.touch = touch;
+					$scope.$apply();
+			},
 
-        cancel: function(touch) {
-          $scope.touch = touch;  
-          $scope.$apply();
-        },
+			cancel: function(touch) {
+					$scope.touch = touch;  
+					$scope.$apply();
+			},
 
-        move: function(touch) {
-          $scope.touch = touch;
-          $scope.$apply();
-        },
+			move: function(touch) {
+					$scope.touch = touch;
+					$scope.$apply();
+			},
 
-        end: function(touch) {
-          $scope.touch = touch;
-          $scope.$apply();
-        }
-      });
-    }
-  };
+			end: function(touch) {
+					$scope.touch = touch;
+					$scope.$apply();
+			}
+	});
+	}
+};
 }]);
 
 //
 // `$drag` example: drag to dismiss
 //
 app.directive('dragToDismiss', function($drag, $parse, $timeout){
-  return {
-    restrict: 'A',
-    compile: function(elem, attrs) {
-      var dismissFn = $parse(attrs.dragToDismiss);
-      return function(scope, elem){
-        var dismiss = false;
+	return {
+		restrict: 'A',
+		compile: function(elem, attrs) {
+			var dismissFn = $parse(attrs.dragToDismiss);
+			return function(scope, elem){
+				var dismiss = false;
 
-        $drag.bind(elem, {
-          transform: $drag.TRANSLATE_RIGHT,
-          move: function(drag) {
-            if( drag.distanceX >= drag.rect.width / 4) {
-              dismiss = true;
-              elem.addClass('dismiss');
-            } else {
-              dismiss = false;
-              elem.removeClass('dismiss');
-            }
-          },
-          cancel: function(){
-            elem.removeClass('dismiss');
-          },
-          end: function(drag) {
-            if (dismiss) {
-              elem.addClass('dismitted');
-              $timeout(function() { 
-                scope.$apply(function() {
-                  dismissFn(scope);  
-                });
-              }, 300);
-            } else {
-              drag.reset();
-            }
-          }
-        });
-      };
-    }
-  };
+				$drag.bind(elem, {
+					transform: $drag.TRANSLATE_RIGHT,
+					move: function(drag) {
+						if( drag.distanceX >= drag.rect.width / 4) {
+							dismiss = true;
+							elem.addClass('dismiss');
+					} else {
+							dismiss = false;
+							elem.removeClass('dismiss');
+					}
+			},
+			cancel: function(){
+				elem.removeClass('dismiss');
+		},
+		end: function(drag) {
+				if (dismiss) {
+					elem.addClass('dismitted');
+					$timeout(function() { 
+						scope.$apply(function() {
+							dismissFn(scope);  
+					});
+				}, 300);
+			} else {
+					drag.reset();
+			}
+	}
+});
+		};
+}
+};
 });
 
 //
@@ -123,128 +124,128 @@ app.directive('dragToDismiss', function($drag, $parse, $timeout){
 // a touch enabled "deck of cards" carousel. See `carousel.html` for markup.
 //
 app.directive('carousel', function(){
-  return {
-    restrict: 'C',
-    scope: {},
-    controller: function() {
-      this.itemCount = 0;
-      this.activeItem = null;
+	return {
+		restrict: 'C',
+		scope: {},
+		controller: function() {
+			this.itemCount = 0;
+			this.activeItem = null;
 
-      this.addItem = function(){
-        var newId = this.itemCount++;
-        this.activeItem = this.itemCount === 1 ? newId : this.activeItem;
-        return newId;
-      };
+			this.addItem = function(){
+				var newId = this.itemCount++;
+				this.activeItem = this.itemCount === 1 ? newId : this.activeItem;
+				return newId;
+		};
 
-      this.next = function(){
-        this.activeItem = this.activeItem || 0;
-        this.activeItem = this.activeItem === this.itemCount - 1 ? 0 : this.activeItem + 1;
-      };
+		this.next = function(){
+				this.activeItem = this.activeItem || 0;
+				this.activeItem = this.activeItem === this.itemCount - 1 ? 0 : this.activeItem + 1;
+		};
 
-      this.prev = function(){
-        this.activeItem = this.activeItem || 0;
-        this.activeItem = this.activeItem === 0 ? this.itemCount - 1 : this.activeItem - 1;
-      };
-    }
-  };
+		this.prev = function(){
+				this.activeItem = this.activeItem || 0;
+				this.activeItem = this.activeItem === 0 ? this.itemCount - 1 : this.activeItem - 1;
+		};
+}
+};
 });
 
 app.directive('carouselItem', function($drag) {
-  return {
-    restrict: 'C',
-    require: '^carousel',
-    scope: {},
-    transclude: true,
-    template: '<div class="item"><div ng-transclude></div></div>',
-    link: function(scope, elem, attrs, carousel) {
-      scope.carousel = carousel;
-      var id = carousel.addItem();
-      
-      var zIndex = function(){
-        var res = 0;
-        if (id === carousel.activeItem){
-          res = 2000;
-        } else if (carousel.activeItem < id) {
-          res = 2000 - (id - carousel.activeItem);
-        } else {
-          res = 2000 - (carousel.itemCount - 1 - carousel.activeItem + id);
-        }
-        return res;
-      };
+	return {
+		restrict: 'C',
+		require: '^carousel',
+		scope: {},
+		transclude: true,
+		template: '<div class="item"><div ng-transclude></div></div>',
+		link: function(scope, elem, attrs, carousel) {
+			scope.carousel = carousel;
+			var id = carousel.addItem();
+			
+			var zIndex = function(){
+				var res = 0;
+				if (id === carousel.activeItem){
+					res = 2000;
+			} else if (carousel.activeItem < id) {
+					res = 2000 - (id - carousel.activeItem);
+			} else {
+					res = 2000 - (carousel.itemCount - 1 - carousel.activeItem + id);
+			}
+			return res;
+	};
 
-      scope.$watch(function(){
-        return carousel.activeItem;
-      }, function(){
-        elem[0].style.zIndex = zIndex();
-      });
-      
-      $drag.bind(elem, {
-        //
-        // This is an example of custom transform function
-        //
-        transform: function(element, transform, touch) {
-          // 
-          // use translate both as basis for the new transform:
-          // 
-          var t = $drag.TRANSLATE_BOTH(element, transform, touch);
-          
-          //
-          // Add rotation:
-          //
-          var Dx    = touch.distanceX, 
-              t0    = touch.startTransform, 
-              sign  = Dx < 0 ? -1 : 1,
-              angle = sign * Math.min( ( Math.abs(Dx) / 700 ) * 30 , 30 );
-          
-          t.rotateZ = angle + (Math.round(t0.rotateZ));
-          
-          return t;
-        },
-        move: function(drag){
-          if(Math.abs(drag.distanceX) >= drag.rect.width / 4) {
-            elem.addClass('dismiss');  
-          } else {
-            elem.removeClass('dismiss');  
-          }
-        },
-        cancel: function(){
-          elem.removeClass('dismiss');
-        },
-        end: function(drag) {
-          elem.removeClass('dismiss');
-          if(Math.abs(drag.distanceX) >= drag.rect.width / 4) {
-            scope.$apply(function() {
-              carousel.next();
-            });
-          }
-          drag.reset();
-        }
-      });
-    }
-  };
+	scope.$watch(function(){
+		return carousel.activeItem;
+}, function(){
+		elem[0].style.zIndex = zIndex();
+});
+	
+	$drag.bind(elem, {
+				//
+				// This is an example of custom transform function
+				//
+				transform: function(element, transform, touch) {
+					// 
+					// use translate both as basis for the new transform:
+					// 
+					var t = $drag.TRANSLATE_BOTH(element, transform, touch);
+					
+					//
+					// Add rotation:
+					//
+					var Dx    = touch.distanceX, 
+					t0    = touch.startTransform, 
+					sign  = Dx < 0 ? -1 : 1,
+					angle = sign * Math.min( ( Math.abs(Dx) / 700 ) * 30 , 30 );
+					
+					t.rotateZ = angle + (Math.round(t0.rotateZ));
+					
+					return t;
+			},
+			move: function(drag){
+					if(Math.abs(drag.distanceX) >= drag.rect.width / 4) {
+						elem.addClass('dismiss');  
+				} else {
+						elem.removeClass('dismiss');  
+				}
+		},
+		cancel: function(){
+			elem.removeClass('dismiss');
+	},
+	end: function(drag) {
+			elem.removeClass('dismiss');
+			if(Math.abs(drag.distanceX) >= drag.rect.width / 4) {
+				scope.$apply(function() {
+					carousel.next();
+			});
+		}
+		drag.reset();
+}
+});
+}
+};
 });
 
 app.directive('dragMe', ['$drag', function($drag){
-  return {
-    controller: function($scope, $element) {
-      $drag.bind($element, 
-        {
-          //
-          // Here you can see how to limit movement 
-          // to an element
-          //
-          transform: $drag.TRANSLATE_INSIDE($element.parent()),
-          end: function(drag) {
-            // go back to initial position
-            drag.reset();
-          }
-        },
-        { // release touch when movement is outside bounduaries
-          sensitiveArea: $element.parent()
-        }
-      );
-    }
-  };
+	return {
+		controller: function($scope, $element) {
+			$drag.bind($element, 
+			{
+					//
+					// Here you can see how to limit movement 
+					// to an element
+					//
+					transform: $drag.TRANSLATE_INSIDE($element.parent()),
+					end: function(drag) {
+						// go back to initial position
+						drag.reset();
+				}
+		},
+				{ // release touch when movement is outside bounduaries
+					sensitiveArea: $element.parent()
+			}
+			);
+	}
+};
 }]);
 
 //
@@ -253,95 +254,95 @@ app.directive('dragMe', ['$drag', function($drag){
 //
 app.controller('MainController', function($rootScope, $scope){
 
-  $scope.swiped = function(direction) {
-    alert('Swiped ' + direction);
-  };
+	$scope.swiped = function(direction) {
+		alert('Swiped ' + direction);
+};
 
-  // User agent displayed in home page
-  $scope.userAgent = navigator.userAgent;
-  
-  // Needed for the loading screen
-  $rootScope.$on('$routeChangeStart', function(){
-    $rootScope.loading = true;
-  });
+	// User agent displayed in home page
+	$scope.userAgent = navigator.userAgent;
+	
+	// Needed for the loading screen
+	$rootScope.$on('$routeChangeStart', function(){
+		$rootScope.loading = true;
+});
 
-  $rootScope.$on('$routeChangeSuccess', function(){
-    $rootScope.loading = false;
-  });
+	$rootScope.$on('$routeChangeSuccess', function(){
+		$rootScope.loading = false;
+});
 
-  // Fake text i used here and there.
-  $scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
+	// Fake text i used here and there.
+	$scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
 
-  // 
-  // 'Scroll' screen
-  // 
-  var scrollItems = [];
+	// 
+	// 'Scroll' screen
+	// 
+	var scrollItems = [];
 
-  for (var i=1; i<=100; i++) {
-    scrollItems.push('Item ' + i);
-  }
+	for (var i=1; i<=100; i++) {
+		scrollItems.push('Item ' + i);
+}
 
-  $scope.scrollItems = scrollItems;
+$scope.scrollItems = scrollItems;
 
-  $scope.bottomReached = function() {
-    /* global alert: false; */
-    alert('Congrats you scrolled to the end of the list!');
-  };
+$scope.bottomReached = function() {
+		/* global alert: false; */
+		alert('Congrats you scrolled to the end of the list!');
+};
 
-  // 
-  // Right Sidebar
-  // 
-  $scope.chatUsers = [
-    { name: 'Carlos  Flowers', online: true },
-    { name: 'Byron Taylor', online: true },
-    { name: 'Jana  Terry', online: true },
-    { name: 'Darryl  Stone', online: true },
-    { name: 'Fannie  Carlson', online: true },
-    { name: 'Holly Nguyen', online: true },
-    { name: 'Bill  Chavez', online: true },
-    { name: 'Veronica  Maxwell', online: true },
-    { name: 'Jessica Webster', online: true },
-    { name: 'Jackie  Barton', online: true },
-    { name: 'Crystal Drake', online: false },
-    { name: 'Milton  Dean', online: false },
-    { name: 'Joann Johnston', online: false },
-    { name: 'Cora  Vaughn', online: false },
-    { name: 'Nina  Briggs', online: false },
-    { name: 'Casey Turner', online: false },
-    { name: 'Jimmie  Wilson', online: false },
-    { name: 'Nathaniel Steele', online: false },
-    { name: 'Aubrey  Cole', online: false },
-    { name: 'Donnie  Summers', online: false },
-    { name: 'Kate  Myers', online: false },
-    { name: 'Priscilla Hawkins', online: false },
-    { name: 'Joe Barker', online: false },
-    { name: 'Lee Norman', online: false },
-    { name: 'Ebony Rice', online: false }
-  ];
+	// 
+	// Right Sidebar
+	// 
+	$scope.chatUsers = [
+	{ name: 'Carlos  Flowers', online: true },
+	{ name: 'Byron Taylor', online: true },
+	{ name: 'Jana  Terry', online: true },
+	{ name: 'Darryl  Stone', online: true },
+	{ name: 'Fannie  Carlson', online: true },
+	{ name: 'Holly Nguyen', online: true },
+	{ name: 'Bill  Chavez', online: true },
+	{ name: 'Veronica  Maxwell', online: true },
+	{ name: 'Jessica Webster', online: true },
+	{ name: 'Jackie  Barton', online: true },
+	{ name: 'Crystal Drake', online: false },
+	{ name: 'Milton  Dean', online: false },
+	{ name: 'Joann Johnston', online: false },
+	{ name: 'Cora  Vaughn', online: false },
+	{ name: 'Nina  Briggs', online: false },
+	{ name: 'Casey Turner', online: false },
+	{ name: 'Jimmie  Wilson', online: false },
+	{ name: 'Nathaniel Steele', online: false },
+	{ name: 'Aubrey  Cole', online: false },
+	{ name: 'Donnie  Summers', online: false },
+	{ name: 'Kate  Myers', online: false },
+	{ name: 'Priscilla Hawkins', online: false },
+	{ name: 'Joe Barker', online: false },
+	{ name: 'Lee Norman', online: false },
+	{ name: 'Ebony Rice', online: false }
+	];
 
-  //
-  // 'Forms' screen
-  //  
-  $scope.rememberMe = true;
-  $scope.email = 'me@example.com';
-  
-  $scope.login = function() {
-    alert('You submitted the login form');
-  };
+	//
+	// 'Forms' screen
+	//  
+	$scope.rememberMe = true;
+	$scope.email = 'me@example.com';
+	
+	$scope.login = function() {
+		alert('You submitted the login form');
+};
 
-  // 
-  // 'Drag' screen
-  // 
-  $scope.notices = [];
-  
-  for (var j = 0; j < 10; j++) {
-    $scope.notices.push({icon: 'envelope', message: 'Notice ' + (j + 1) });
-  }
+	// 
+	// 'Drag' screen
+	// 
+	$scope.notices = [];
+	
+	for (var j = 0; j < 10; j++) {
+		$scope.notices.push({icon: 'envelope', message: 'Notice ' + (j + 1) });
+}
 
-  $scope.deleteNotice = function(notice) {
-    var index = $scope.notices.indexOf(notice);
-    if (index > -1) {
-      $scope.notices.splice(index, 1);
-    }
-  };
+$scope.deleteNotice = function(notice) {
+		var index = $scope.notices.indexOf(notice);
+		if (index > -1) {
+			$scope.notices.splice(index, 1);
+	}
+};
 });
